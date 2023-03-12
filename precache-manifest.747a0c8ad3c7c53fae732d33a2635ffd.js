daa419278085fd878b1d5cd0b7696c63b0835b97
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2023-03-12 13:49:34 +0530

    Updates

---
+++ precache-manifest.747a0c8ad3c7c53fae732d33a2635ffd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2629b9d602aaa9994c",
+    "url": "/Keeper/static/js/main.ab2629b9.chunk.js"
+  },
+  {
+    "revision": "f2403345516cebc62c50",
+    "url": "/Keeper/static/js/1.f2403345.chunk.js"
+  },
+  {
+    "revision": "47e838a1f9c8a8d90fc4",
+    "url": "/Keeper/static/js/runtime~main.47e838a1.js"
+  },
+  {
+    "revision": "69216989c5287ba6ca69d657d2c98c93",
+    "url": "/Keeper/index.html"
+  }
+];
