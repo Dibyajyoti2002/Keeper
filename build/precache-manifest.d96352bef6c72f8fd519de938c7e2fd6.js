71cfa68f7b17633c8fc0ad93fe8bbaf38f75dac8
Dibyajyoti Ganguly <gangulyd49@gmail.com>
2023-03-12 13:45:16 +0530

    Trial commit

---
+++ build/precache-manifest.d96352bef6c72f8fd519de938c7e2fd6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab2629b9d602aaa9994c",
+    "url": "/Keeper/static/js/main.ab2629b9.chunk.js"
+  },
+  {
+    "revision": "f2403345516cebc62c50",
+    "url": "/Keeper/static/js/1.f2403345.chunk.js"
+  },
+  {
+    "revision": "47e838a1f9c8a8d90fc4",
+    "url": "/Keeper/static/js/runtime~main.47e838a1.js"
+  },
+  {
+    "revision": "d823e595fdbe3d4a7ad3aaeb101d3030",
+    "url": "/Keeper/index.html"
+  }
+];
